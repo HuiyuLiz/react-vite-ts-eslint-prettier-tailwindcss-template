eead7b759910897dbe818ec1b357a8569edac623
Huiyu Li <curlyuu@gmail.com>
2024-03-20 00:15:31 +0800

    chore: set up linting and formatting for code
    add eslint、prettier、husky、commitlint、Tailwind CSS

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,53 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
   },
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
+  rules: {
+    indent: [2, 2],
+    'max-len': [2, { code: 150 }],
+    'react/jsx-max-props-per-line': [2, { maximum: { single: 3, multi: 1 } }],
+    'react/jsx-wrap-multilines': 2,
+    semi: 2,
+    'no-var': 'error',
+    'no-undef': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-no-target-blank': 'off',
+    '@typescript-eslint/explicit-function-return-type': ['off'],
+    '@typescript-eslint/no-non-null-assertion': 'off'
+  }
 }
